53fd5e0c8ce25afea30a7a9b51e16370a64b1b2b
Junxiao Shi <git@mail1.yoursunny.com>
2019-09-07 11:28:16 -0400

    ndnping page

---
+++ usr/lib/lua/luci/controller/admin/named-data.lua
@@ -0,0 +1,34 @@
+module("luci.controller.admin.named-data", package.seeall)
+
+function index()
+  page = node("admin", "named-data")
+  page.target = firstchild()
+  page.title  = _("NDN")
+  page.order  = 80
+  page.index  = true
+
+  page = node("admin", "named-data", "ndnping")
+  page.target = template("named-data/ndnping")
+  page.title  = _("ndnping")
+  page.order  = 60
+
+  page = entry({"admin", "named-data", "diag_ndnping"}, post("diag_ndnping"), nil)
+  page.leaf = true
+end
+
+function diag_ndnping(cmd)
+  local prefix = luci.http.formvalue("q")
+  luci.http.prepare_content("text/plain")
+
+  local util = io.popen("ndnping -c 4 %s" % luci.util.shellquote(prefix))
+  if util then
+    while true do
+      local ln = util:read("*l")
+      if not ln then break end
+      luci.http.write(ln)
+      luci.http.write("\n")
+    end
+
+    util:close()
+  end
+end
