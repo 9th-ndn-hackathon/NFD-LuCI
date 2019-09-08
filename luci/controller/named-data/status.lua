b74333e7747b2c6f9aaedbc1c3fa9ef5190683d0
Junxiao Shi <git@mail1.yoursunny.com>
2019-09-08 08:30:54 -0400

    link to traditional nfd-status page

--- luci/controller/named-data/status.lua
+++ luci/controller/named-data/status.lua
@@ -4,13 +4,18 @@ function index()
   page = node("admin", "status", "named-data")
   page.target = firstchild()
   page.title  = _("NDN")
-  page.order  = 20
+  page.order  = 70
   page.index  = true
+
+  page = node("admin", "status", "named-data", "general")
+  page.target = template("named-data/status-general")
+  page.title  = _("NFD General Status")
+  page.order  = 10
 
   page = node("admin", "status", "named-data", "faces")
   page.target = template("named-data/status-faces")
   page.title  = _("NFD Faces")
-  page.order  = 10
+  page.order  = 20
 
   page = entry({"admin", "status", "named-data", "create_face"}, post("create_face"), nil)
   page.leaf = true
