5a561d9f52f6f68cfab7129a60438ace8a50d8c9
Hunter <hdellaverson@gmail.com>
2019-09-08 11:48:31 -0400

    global counters

--- luci/controller/named-data/status.lua
+++ luci/controller/named-data/status.lua
@@ -12,6 +12,9 @@ function index()
   page.title  = _("NFD Status")
   page.order  = 10
 
+  page = entry({"admin", "named-data", "displayCountersNDN"}, post("displayCountersNDN"), nil)
+  page.leaf = true
+
   page = node("admin", "status", "named-data", "faces")
   page.target = template("named-data/status-faces")
   page.title  = _("Faces")
@@ -22,6 +25,7 @@ function index()
   page.title  = _("Routes")
   page.order  = 30
 
+  
   page = entry({"admin", "status", "named-data", "create_face"}, post("create_face"), nil)
   page.leaf = true
 end
@@ -32,3 +36,53 @@ function create_face()
   luci.http.redirect("faces")
 end
 
+
+function displayCountersNDN(cmd)
+
+  local prefix = luci.http.formvalue("q")
+  luci.http.prepare_content("text/plain")
+
+  local util = io.popen("nfdc status show")
+  if util then
+    local mapper = {}
+    while true do
+      local ln = util:read("*l")
+      s = ltrim(tostring(ln))
+      local splitline = split(tostring(s), "=")
+
+      if (#(splitline) > 1) then
+      mapper[tostring(splitline[1])] = tostring(splitline[2])
+
+    end 
+
+      if not ln then break end
+    end
+    desiredList = {"nFibEntries", "nPitEntries", "nMeasurementEntries", "nCsEntries", "nInInterests", "nOutInterests", "nInData", "nOutData", "nInNacks","nOutNacks", "nSatisfiedInterests","nUnsatisfiedInterests"}
+    dictSize = #(desiredList)
+    mapSubset = {}
+    for i = 1, dictSize, 1 do
+      mapSubset[desiredList[i]] = mapper[desiredList[i]]
+    end
+
+    luci.http.write(luci.json.encode(mapSubset))
+    util:close()
+  end
+
+end
+
+
+--not using luci's split function because when used on the equals sign as relevant for grabbing counters only grabs the first half e.g. "nFibEntries:"
+
+function split(s, sep)
+  local fields = {}
+
+  local sep = sep or " "
+  local pattern = string.format("([^%s]+)", sep)
+  string.gsub(s, pattern, function(c) fields[#fields + 1] = c end)
+
+  return fields
+end
+
+function ltrim(s)
+  return (s:gsub("^%s*", ""))
+end
